2a45c1015e81960aa586b7e6d97e447b70055938
Sojinoa <sojinoa06@google.com>
2024-06-11 22:44:28 +0800

    added initial heroku plugins

--- src/main/java/sojinoa/github/api/ApidemoApplication.java
+++ src/main/java/sojinoa/github/api/ApidemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.api.apidemo;
+package sojinoa.github.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/sojinoa/github/api/controller/HelloController.java
+++ src/main/java/sojinoa/github/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.api.apidemo.controller;
+package sojinoa.github.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
